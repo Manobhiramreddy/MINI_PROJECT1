6572e22f969b6e3a447c6fa1857ee89515cc0e78
ManobhiramReddy <f62galaxy@gmail.com>
2024-05-10 06:46:30 +0530

    Your message about the commit

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const mongoose = require('mongoose');
 const connect = mongoose.connect("mongodb://localhost:27017/Login-tut");
 
@@ -24,4 +25,32 @@ const Loginschema = new mongoose.Schema({
 // collection part
 const collection = new mongoose.model("users", Loginschema);
 
+=======
+const mongoose = require('mongoose');
+const connect = mongoose.connect("mongodb://localhost:27017/Login-tut");
+
+// Check database connected or not
+connect.then(() => {
+    console.log("Database Connected Successfully");
+})
+.catch(() => {
+    console.log("Database cannot be Connected");
+})
+
+// Create Schema
+const Loginschema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// collection part
+const collection = new mongoose.model("users", Loginschema);
+
+>>>>>>> 89a435b (Your message about the commit)
 module.exports = collection;
